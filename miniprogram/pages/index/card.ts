c401e56e7e8e63f8b7f09f8edd9268b582b79161
余力 <li1.yu@BKJK.COM>
2020-01-20 17:12:34 +0800

    v2.0

--- miniprogram/pages/index/card.ts
+++ miniprogram/pages/index/card.ts
@@ -231,9 +231,265 @@ export const card = [{
 {
   en: '(Organic)machinery',
   zh: '（有机）机械'
-  },
-  {
-    en: 'Courage!',
-    zh: '勇气！'
-  }
+},
+{
+  en: 'Courage!',
+  zh: '勇气！'
+},
+{
+  en: 'What mistakes did you make last time?',
+  zh: '你上次犯了什么错？'
+},
+{
+  en: 'You are an engineer',
+  zh: '你是一个工程师'
+},
+{
+  en: 'Consider different fading system',
+  zh: '考虑不同的渐变体系'
+},
+{
+  en: 'Remove ambiguities and convert to specifics',
+  zh: '消除冲突，并转化为细节'
+},
+{
+  en: 'Mute and continue',
+  zh: '静音，继续'
+},
+{
+  en: 'Look at the order in which you do things',
+  zh: '看看你的顺序'
+},
+{
+  en: 'It is quite possible(after all)',
+  zh: '总有办法'
+},
+{
+  en: 'Go outside. Shut the door',
+  zh: '出去走走。关上门'
+},
+{
+  en: 'Do not stress one thing more than another',
+  zh: '不要过分强调一件事'
+},
+{
+  en: 'Do we need holes?',
+  zh: '需要停顿吗？'
+},
+{
+  en: 'Cluster analysis',
+  zh: '聚类分析'
+},
+{
+  en: 'Work at a different speed',
+  zh: '换个速度'
+},
+{
+  en: 'Do something boring',
+  zh: '做点无聊的'
+},
+{
+  en: 'Look closely at the most embarrassing details and amplify them',
+  zh: '仔细研究最令人尴尬的细节并加以放大'
+},
+{
+  en: 'Define an area as SAFE and use it as an anchor',
+  zh: '定义一个舒适圈作为基点'
+},
+{
+  en: 'Mechanicalize something idiosyncratic',
+  zh: '机械化一些特质'
+},
+{
+  en: 'Overtly resist change',
+  zh: '公开的对抗变化'
+},
+{
+  en: 'Emphasize the flaws',
+  zh: '强调缺陷'
+},
+{
+  en: 'Accept advice',
+  zh: '接受建议'
+},
+{
+  en: 'Remember those quiet evenings',
+  zh: '记住那些安静的夜晚'
+},
+{
+  en: 'Take a break',
+  zh: '休息一下'
+},
+{
+  en: 'The tape is now the music',
+  zh: '磁带是现在的音乐'
+},
+{
+  en: 'Short circuit',
+  zh: '短路'
+},
+{
+  en: 'Imagine the music as a moving chain or caterpillar',
+  zh: '想象音乐就像一条运动的链条或毛毛虫'
+},
+{
+  en: 'Use an old idea',
+  zh: '用一个老的想法'
+},
+// {
+//   en: 'Intentions -credibility of -nobility of -humility of',
+//   zh: '用一个老的想法'
+// }
+{
+  en: 'Destroy -nothing -the most important thing',
+  zh: '什么也不摧毁，或摧毁最重要的'
+},
+{
+  en: 'Imagine the music as a set of disconnected events',
+  zh: '将音乐想象成一系列断开的事件'
+},
+{
+  en: 'Change nothing and continue with immaculate consistency',
+  zh: '不做任何变动，保持完美一致性'
+},
+{
+  en: 'What are you really thinking about just now? Incorporate',
+  zh: '你现在在想什么？合并'
+},
+{
+  en: 'Children’s voices -speaking -singing',
+  zh: '孩子在说话，孩子在唱歌'
+},
+{
+  en: 'Assemble some of the instruments in a group and treat the group',
+  zh: '把一些乐器合并成一组'
+},
+{
+  en: 'Feedback recordings into an acoustic situation',
+  zh: '反馈录音进一个声学环境'
+},
+{
+  en: 'Shut the door and listen from outside',
+  zh: '关上门，从外面听'
+},
+{
+  en: 'Towards the insignificant',
+  zh: '迈向微不足道'
+},
+{
+  en: 'Is the tuning appropriate？',
+  zh: '在调上吗？'
+},
+{
+  en: 'Simply a matter of work',
+  zh: '拼体力吧'
+},
+{
+  en: 'Look at a very small object, look at its centre',
+  zh: '看一个很小的物件，看它的中心'
+},
+{
+  en: 'Not building a wall but making a brick',
+  zh: '不去建一堵墙，尝试做一块砖'
+},
+{
+  en: 'Revaluation(a warm feeling)',
+  zh: '重新评估'
+},
+{
+  en: 'Disciplined self-indulgence',
+  zh: '有纪律的自我放纵'
+},
+{
+  en: 'The most important thing is the thing most easily forgotten',
+  zh: '最重要的也最容易忘记'
+},
+{
+  en: 'Always first steps',
+  zh: '永远是第一步'
+},
+{
+  en: 'Idiot glee',
+  zh: '愚蠢的快乐'
+},
+{
+  en: 'Question the heroic approach',
+  zh: '质疑英雄式的途径'
+},
+{
+  en: 'Be extravagant',
+  zh: '奢侈一点'
+},
+{
+  en: 'Always give yourself credit for having more than personality',
+  zh: '总是给自己以超越个性的信心'
+},
+{
+  en: 'State the problem in words as clearly as possible',
+  zh: '尽可能用语言清楚地陈述问题'
+},
+{
+  en: 'Faced with a choice, do both',
+  zh: '遇到一个选择，我全要'
+},
+{
+  en: 'Tape your mouth',
+  zh: '采样你的嘴'
+},
+{
+  en: 'Twist the spine',
+  zh: '扭一下脊椎'
+},
+{
+  en: 'Get your neck massaged',
+  zh: '按摩一下脖子'
+},
+{
+  en: 'Lowest common denominator check -single beat -single note -single riff',
+  zh: '最小公分母检查，节奏，音符，即兴'
+},
+{
+  en: 'Do the washing up',
+  zh: '清洗'
+},
+{
+  en: 'Listen in total darkness,or in a very large room, very quietly',
+  zh: '在完全黑暗的环境中或在非常大的房间中安静地聆听'
+},
+{
+  en: 'Convert a melodic element into a rhythmic element',
+  zh: '将旋律元素转换成节奏元素'
+},
+{
+  en: 'Would anybody want it?',
+  zh: '会有人想听这个吗？'
+},
+{
+  en: 'Spectrum analysis',
+  zh: '频谱分析'
+},
+{
+  en: 'Retrace your steps',
+  zh: '回溯你的步骤'
+},
+{
+  en: 'Go to an extreme,move back to a more comfortable place',
+  zh: '去到一个极端，然后回到一个更舒服的地方'
+},
+{
+  en: 'Once the search is in progress,something will be found',
+  zh: '一旦开始寻找，总会找到些什么'
+},
+{
+  en: 'Only a part,not the whole',
+  zh: '只是一部分，不是全部'
+},
+{
+  en: 'From nothing to more than nothing',
+  zh: '无中生有'
+},
+{
+  en: 'Be less critical more often',
+  zh: '更少的批评'
+}
 ]
